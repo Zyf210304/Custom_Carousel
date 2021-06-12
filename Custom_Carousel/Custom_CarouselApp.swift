0ba142265250dadffe8b716f747f55cd45e95330
张亚飞 <672233580@qq.com>
2021-06-12 11:01:30 +0800

    Initial Commit

---
+++ Custom_Carousel/Custom_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CarouselApp.swift
+//  Custom_Carousel
+//
+//  Created by 张亚飞 on 2021/6/12.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
